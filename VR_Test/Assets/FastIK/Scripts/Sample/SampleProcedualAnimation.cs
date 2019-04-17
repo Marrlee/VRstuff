28db8125ecf9e8dbfa3835b82124d33f5c0bb588
Martin Lee <33700647+Marrlee@users.noreply.github.com>
2019-04-17 00:03:27 -0700

    VR Game

---
+++ VR_Test/Assets/FastIK/Scripts/Sample/SampleProcedualAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+using Valve.VR;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+ public class SampleProcedualAnimation :  MonoBehaviour
+    {
+    public GameObject TargetHandLeft;
+    public GameObject TargetHandRight;
+
+    void Start()
+    {
+      
+    }
+     void Update()
+        {
+
+        TargetHandLeft.transform.SetPositionAndRotation(
+            InputTracking.GetLocalPosition(XRNode.LeftHand),
+            InputTracking.GetLocalRotation(XRNode.LeftHand));
+        }
+
+    }
+
